1c0a57813d48622b64c52ade0643a1548aa19a85
ryan <ryan@arrayofstars.com>
2022-12-06 15:51:36 -0500

    add image display to each product

--- pages/index.vue
+++ pages/index.vue
@@ -9,35 +9,40 @@ const { data } = await useAsyncData('product', async(nuxtApp) => {
 })
 
 const items = data.value.items;
-console.log(items);
+// console.log(items);
 </script>
 
 <template>
 <main>
   <h1>Ryan's Garage Sale</h1>
-  <div v-for="product in items"> 
+  <div class="product-container" v-for="product in items"> 
     <h1>{{product.fields.title}}</h1>
     <p>{{product.fields.price}}</p>
     <p>{{product.fields.description.content[0].content[0].value}}</p>
+    <img 
+    v-if="product.fields.image"
+    :src="`${product.fields.image.fields.file.url}`" 
+    />
   </div>
-  <p></p>
-  <!-- <table border="1 px solid">
-    <thead>
-      <tr>
-        <th>Name</th>
-        <th>Symbol</th>
-        <th>Price</th>
-        <th>Details</th>
-      </tr>
-    </thead>
-    <tr v-for="currency in data.data" :key="data.data.id">
-      <td>{{ currency.name }}</td>
-      <td>{{ currency.symbol }}</td>
-      <td>{{ currency.price_usd }}</td>
-      <td>
-        <NuxtLink :to="('/currency/' + currency.id)"> {{ currency.id }}</NuxtLink>
-      </td>
-    </tr>
-  </table> -->
 </main>
 </template>
+
+<style scoped>
+.product-container {
+  padding: 20px;
+  width: 250px;
+  cursor: pointer;
+  border: 1px solid #39495c;
+  border-radius: 20px;
+  margin-bottom: 18px;
+}
+
+.product-container:hover {
+  transform: scale(1.01);
+  box-shadow: 0 3px 12px 0 rgba(0, 0, 0, 0.2);
+}
+img {
+  height: 200px;
+  width: 250px;
+}
+</style>
