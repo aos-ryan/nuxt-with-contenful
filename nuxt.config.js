c17ff2449b53d00603e6f331b0b2b78518b51dae
ryan <ryan@arrayofstars.com>
2022-12-08 14:47:12 -0500

    graphql in progress implementation

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,9 +1,11 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  modules: ['nuxt-graphql-client'],
   runtimeConfig: {
     public: {
       CTF_SPACE_ID: process.env.NUXT_CTF_SPACE_ID,
-      CTF_ACCESS_TOKEN: process.env.NUXT_CTF_CDA_ACCESS_TOKEN
+      CTF_ACCESS_TOKEN: process.env.NUXT_CTF_CDA_ACCESS_TOKEN,
+      GQL_HOST: process.env.NUXT_GQL_HOST,
     }
   },
   app: {
--- pages/item/[id].vue
+++ pages/item/[id].vue
@@ -1,24 +1,22 @@
 <script setup>
 const route = useRoute();
 
-const { data } = await useAsyncData('product', async(nuxtApp) => {
-  const { $contentfulClient } = nuxtApp
-  return await $contentfulClient.getEntry(route.params.id)
-});
-const product = data.value;
+const { data } = await useAsyncGql('productEntryQuery')
+// const product = data.value;
 
 </script>
 
 <template>
 <main>
   <div>
-    <h1>{{product.fields.title}}</h1>
+    <p>Data: {{data}}</p>
+    <!-- <h1>{{product.fields.title}}</h1>
     <img
     v-if="product.fields.image" 
     :src="`${product.fields.image?.fields.file.url}`"
     />
     <p>Price: {{product.fields.price}}</p>
-    <p>Description: {{product.fields.description.content[0].content[0].value}}</p>
+    <p>Description: {{product.fields.description.content[0].content[0].value}}</p> -->
   </div>
 </main>
 </template>
